fabd12273733a1b20b6ac7558d6ba93dc6e5f8c8
mmmjunjoy <sjbssk0809@gmail.com>
2023-02-06 11:45:21 +0900

    feat: react component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
-import Hello from './components/Hello'
-function App() {
-  return (
-    <div className="App">
-      <Hello/>
-    </div>
-  );
+import React ,{Component} from 'react';
+// import Hello from './components/Hello'
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <div>
+          <span>hello junjoy</span>
+        </div>
+        <SecondComponent/>
+
+      </div>
+    );
+  }
 }
+
+
+class SecondComponent extends Component{
+  render(){
+    return(
+      <div>
+        <span> i am second component </span>
+      </div>
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Hello/>
+//     </div>
+//   );
+// }
 
 export default App;
--- first-app/src/components/Hello.js
+++ first-app/src/components/Hello.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import {useState} from 'react'
+// import React from 'react'
+// import {useState} from 'react'
 
-export default function Hello() {
-const [count,setCount] = useState(0)
-const onCount = ()=>{
-   setCount(count+1)
-}
-// function onCount2(){
-//     console.log("onCount2")
+// export default function Hello() {
+// const [count,setCount] = useState(0)
+// const onCount = ()=>{
+//    setCount(count+1)
 // }
-  return (
-    <>
-        <div>{count}</div>
-        <button onClick={onCount}>버튼</button>
-    </>
-  )
-}
+// // function onCount2(){
+// //     console.log("onCount2")
+// // }
+//   return (
+//     <>
+//         <div>{count}</div>
+//         <button onClick={onCount}>버튼</button>
+//     </>
+//   )
+// }

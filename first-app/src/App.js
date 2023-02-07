3736c628aa9c9ae37509de910f4c539411f9a420
mmmjunjoy <sjbssk0809@gmail.com>
2023-02-07 14:00:08 +0900

    feat:SCSS- Button ,div

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,64 +1,95 @@
 import './App.css';
 import React ,{Component} from 'react';
+import styled from 'styled-components';
 
 // import Header from './components/header';
 // import Hello2 from './components/Hello2';
 
 
+// ' 와 ` 의 차이는 매우 다르니 조심할 것 
+
+const Button = styled.button`
+  background: ${props => (props.primary ? 'red':'black')};
+  color: ${props => (props.primary ? 'black' : 'palevioletred' )};
+
+`;
+
 class App extends Component{
-  constructor(props){
-    super(props);
-    console.log('constructor');
-  }
-  componentDidMount(){
-    console.log('componentDidMount');
-
-
-  }
-
-  componentDidUpdate(){
-    console.log("componentDidUpdate");
-
-  }
-  componentWillUnmount(){
-    console.log('componentWillUnmount');
-
-  }
-
   render(){
-    console.log('render');
+    console.log('App: render');
 
-    return (
-      <div className='App'> 
+
+    return(
+      <div className='App'>
         Life cycle test
-        <Child/>
+        <Button> not primary</Button>
+        <Button primary> primary </Button>
       </div>
     );
   }
 }
-class Child extends Component {
-  constructor(props) {
-    super(props);
-    console.log('Child: constructor');
-  }
-  componentDidMount() {
-    console.log('Child: componentDidMount');
-  }
-  componentDidUpdate() {
-    console.log('Child: componentDidUpdate');
-  }
-  componentWillUnmount() {
-    console.log('Child: componentWillUnmount');
-  }
-  render() {
-    console.log('Child: render');
 
-    return <div className="App">Life cycle test</div>;
-  }
-}
+
+
+
 
 
 export default App;
+
+
+// console.log 체험하기 (componentDidMount,componentDidUpdate 등등)
+//   constructor(props){
+//     super(props);
+//     console.log('constructor');
+//   }
+//   componentDidMount(){
+//     console.log('componentDidMount');
+
+
+//   }
+
+//   componentDidUpdate(){
+//     console.log("componentDidUpdate");
+
+//   }
+//   componentWillUnmount(){
+//     console.log('componentWillUnmount');
+
+//   }
+
+//   render(){
+//     console.log('render');
+
+//     return (
+//       <div className='App'> 
+//         Life cycle test
+//         <Child/>
+//       </div>
+//     );
+//   }
+// }
+// class Child extends Component {
+//   constructor(props) {
+//     super(props);
+//     console.log('Child: constructor');
+//   }
+//   componentDidMount() {
+//     console.log('Child: componentDidMount');
+//   }
+//   componentDidUpdate() {
+//     console.log('Child: componentDidUpdate');
+//   }
+//   componentWillUnmount() {
+//     console.log('Child: componentWillUnmount');
+//   }
+//   render() {
+//     console.log('Child: render');
+
+//     return <div className="App">Life cycle test</div>;
+//   }
+// }
+
+
 
   // state, 현재 컴포턴트의 데이터
 //   state = {
